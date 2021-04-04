d4dc0506f81bb38abc45620c7f7b9cabc14315bf
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-04-04 16:48:48 +0900

    Add PieCahrt to homm

--- app/src/main/java/com/jp_funda/jaiz/ui/home/HomeFragment.java
+++ app/src/main/java/com/jp_funda/jaiz/ui/home/HomeFragment.java
@@ -14,30 +14,28 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.Legend;
+
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.interfaces.datasets.IPieDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.jp_funda.jaiz.R;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private View root;
     private PieChart pieChart;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
@@ -45,8 +43,57 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        createPieChart();
         return root;
     }
 
+    private void createPieChart() {
+        PieChart pieChart = (PieChart) root.findViewById(R.id.home_pie_chart);
 
+        pieChart.setDrawHoleEnabled(true); // 真ん中に穴を空けるかどうか
+        pieChart.setHoleRadius(50f);       // 真ん中の穴の大きさ(%指定)
+        pieChart.setTransparentCircleRadius(55f);
+        pieChart.setRotationAngle(270);          // 開始位置の調整
+        pieChart.setRotationEnabled(true);       // 回転可能かどうか
+        pieChart.getLegend().setEnabled(true);
+        pieChart.setData(createPieChartData());
+
+        // 更新
+        pieChart.invalidate();
+        // アニメーション
+        pieChart.animateXY(2000, 2000); // 表示アニメーション
+    }
+
+    private PieData createPieChartData() {
+        ArrayList<Entry> yVals = new ArrayList<>();
+        ArrayList<String> xVals = new ArrayList<>();
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        xVals.add("A");
+        xVals.add("B");
+        xVals.add("C");
+
+        yVals.add(new Entry(20, 0));
+        yVals.add(new Entry(30, 1));
+        yVals.add(new Entry(50, 2));
+
+        PieDataSet dataSet = new PieDataSet(yVals, "Data");
+        dataSet.setSliceSpace(5f);
+        dataSet.setSelectionShift(1f);
+
+        // 色の設定
+        colors.add(ColorTemplate.COLORFUL_COLORS[0]);
+        colors.add(ColorTemplate.COLORFUL_COLORS[1]);
+        colors.add(ColorTemplate.COLORFUL_COLORS[2]);
+        dataSet.setColors(colors);
+        dataSet.setDrawValues(true);
+
+        PieData data = new PieData(xVals, dataSet);
+        data.setValueFormatter(new PercentFormatter());
+
+        // テキストの設定
+        data.setValueTextSize(12f);
+        data.setValueTextColor(Color.WHITE);
+        return data;
+    }
 }
