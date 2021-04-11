49adb9ea4fd52dd20d039c241bb823ab482d903b
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-04-11 16:40:34 +0900

    Create Current Lesson Status

--- app/src/main/java/com/jp_funda/jaiz/ViewModles/LessonViewModel.java
+++ app/src/main/java/com/jp_funda/jaiz/ViewModles/LessonViewModel.java
@@ -4,10 +4,12 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.jp_funda.jaiz.models.CurrentLessonStatus;
 import com.jp_funda.jaiz.models.Lesson;
 import com.jp_funda.jaiz.models.UserLessonStatus;
 
 public class LessonViewModel extends ViewModel {
     public Lesson lesson;
     public UserLessonStatus lessonStatus;
+    public CurrentLessonStatus currentLessonStatus;
 }
---
+++ app/src/main/java/com/jp_funda/jaiz/models/CurrentLessonStatus.java
@@ -0,0 +1,49 @@
+package com.jp_funda.jaiz.models;
+
+public class CurrentLessonStatus {
+    private int problemNumber;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+
+    public int getProblemNumber() {
+        return problemNumber;
+    }
+
+    public void setProblemNumber(int problemNumber) {
+        this.problemNumber = problemNumber;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public void setAnswer1(String answer1) {
+        this.answer1 = answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public void setAnswer2(String answer2) {
+        this.answer2 = answer2;
+    }
+
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public void setAnswer3(String answer3) {
+        this.answer3 = answer3;
+    }
+
+    public String getAnswer4() {
+        return answer4;
+    }
+
+    public void setAnswer4(String answer4) {
+        this.answer4 = answer4;
+    }
+}
